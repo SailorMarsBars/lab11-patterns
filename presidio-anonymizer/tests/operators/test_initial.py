e6db37668c9af5109ec2d7231cfb62327b671d45
Edsel Witkowski <232106383+SailorMarsBars@users.noreply.github.com>
2025-12-15 06:57:45 +0000

    added test case for extra white-space

--- presidio-anonymizer/tests/operators/test_initial.py
+++ presidio-anonymizer/tests/operators/test_initial.py
@@ -5,11 +5,14 @@ def test_correct_name():
     assert Initial().operator_name() == "initial"
 
 @pytest.mark.parametrize(
-    "input_text, initials",
+    "input_text, expected_initials",
     [
+        # Standard case
         ("John Smith", "J. S."),
+        # New case: Extra whitespaces and leading/trailing spaces
+        ("      Eastern     Michigan    University ", "E. M. U."),
     ],
 )
-def test_given_value_for_initial(input_text, initials):
+def test_given_value_for_initial(input_text, expected_initials):
     text = Initial().operate(input_text)
-    assert text == initials
+    assert text == expected_initials
